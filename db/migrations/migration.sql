1b5a84ecfe590ea24e9c7a83b382b7a9cdc47609
James <jamesjeremysijaya@gmail.com>
2024-07-27 00:22:07 +0700

    update migration

--- db/migrations/migration.sql
+++ db/migrations/migration.sql
@@ -13,6 +13,7 @@ CREATE TABLE IF NOT EXISTS payments
 (
     id         BIGSERIAL PRIMARY KEY,
     name       text                        NOT NULL,
+    image_url  text                        NOT NULL,
     created_at timestamp(3) with time zone NOT NULL default now(),
     updated_at timestamp(3) with time zone,
     deleted_at timestamp(3) with time zone
@@ -20,17 +21,18 @@ CREATE TABLE IF NOT EXISTS payments
 
 CREATE TABLE IF NOT EXISTS products
 (
-    id         BIGSERIAL PRIMARY KEY,
-    game_id    bigint                   not null
+    id          BIGSERIAL PRIMARY KEY,
+    game_id     bigint                   not null
         constraint products_game_id_fkey
             references games (id),
-    name       text                     NOT NULL,
-    price      numeric                  NOT NULL,
-    image_url  text                     NOT NULL,
-    quantity   int                      NOT NULL,
-    created_at timestamp with time zone NOT NULL default now(),
-    updated_at timestamp with time zone,
-    deleted_at timestamp with time zone
+    name        text                     NOT NULL,
+    description text,
+    price       numeric                  NOT NULL,
+    image_url   text                     NOT NULL,
+    quantity    int                      NOT NULL,
+    created_at  timestamp with time zone NOT NULL default now(),
+    updated_at  timestamp with time zone,
+    deleted_at  timestamp with time zone
 );
 
 CREATE TABLE IF NOT EXISTS users
@@ -54,13 +56,15 @@ CREATE TABLE IF NOT EXISTS orders
     product_id     bigint                   not null
         constraint orders_product_id_fkey
             references products (id),
+    uid            text                     NOT NULL,
+    email          text,
     price          numeric                  NOT NULL,
     status_order   text,
     payment_status text,
-    server         text,
     created_at     timestamp with time zone NOT NULL default now(),
     updated_at     timestamp with time zone,
     deleted_at     timestamp with time zone
 );
 
-insert into users(email, password, name) values ('admin@gmail.com', '$2a$10$PoOqxwn8jmeTdGBe3wXc0Oak6BXdue/qFbQOorgg7a3Hk3uST5ibC', 'admin')
+insert into users(email, password, name)
+values ('admin@gmail.com', '$2a$10$PoOqxwn8jmeTdGBe3wXc0Oak6BXdue/qFbQOorgg7a3Hk3uST5ibC', 'admin')
